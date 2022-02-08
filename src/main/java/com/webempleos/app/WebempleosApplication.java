f8391cd15dc02086ec0f02b04c66fc42806f766b
Fede Buchet <fedebuchet17@gmail.com>
2022-02-07 21:43:22 -0300

    inicio del proyecto

---
+++ src/main/java/com/webempleos/app/WebempleosApplication.java
@@ -0,0 +1,13 @@
+package com.webempleos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebempleosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebempleosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webempleos/app/WebempleosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webempleos.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebempleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
