a891f06976fb229f54d2a381c54c47a0dc8589e0
Fede Buchet <fedebuchet17@gmail.com>
2022-02-08 00:06:01 -0300

    agregando la estructura basica de paquetes

--- src/main/java/com/webempleos/app/models/entity/Usuario.java
+++ src/main/java/com/webempleos/app/models/entity/Usuario.java
@@ -1,4 +1,34 @@
 package com.webempleos.app.models.entity;
 
-public class Usuario {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1901544997042742413L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer documento;
+    @Column(name = "fecha_nacimiento")
+    private LocalDate fechaNacimiento;
+    private String nombre;
+    private String apellido;
+    private String username;
+    private String password;
+    private String email;
+    private boolean alta;
+    private Byte[] imagen;
+    private String educacion;
+    private String descripcion;
 }
