69b82de13a08948a51d53183662668b1ed9d2899
Fede Buchet <fedebuchet17@gmail.com>
2022-02-08 22:59:39 -0300

    agregando anotaciones de lombok

--- src/main/java/com/webempleos/app/models/entity/Autoridad.java
+++ src/main/java/com/webempleos/app/models/entity/Autoridad.java
@@ -1,8 +1,15 @@
 package com.webempleos.app.models.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "autoridades")
 public class Autoridad implements Serializable {
