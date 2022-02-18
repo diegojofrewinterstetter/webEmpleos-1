dc9a1db841b14cec295d960d5cb73d47eeb46fd5
Fede Buchet <fedebuchet17@gmail.com>
2022-02-17 23:53:59 -0300

    agregando navbar a todos los html ,y creando el index

--- src/main/java/com/webempleos/app/controllers/InicioController.java
+++ src/main/java/com/webempleos/app/controllers/InicioController.java
@@ -1,7 +1,15 @@
 package com.webempleos.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = {"/", ""})
 public class InicioController {
+
+    @GetMapping(value = {"/", ""})
+    public String inicio() {
+        return "index";
+    }
 }
